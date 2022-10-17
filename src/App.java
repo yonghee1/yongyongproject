5878041aab74571d44e221b911c9019ca8a5d5cf
yongyonglee <dlfhszzz@naver.com>
2022-10-17 13:47:32 +0900

    first commit

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("안녕");
+        System.out.println("깃허브 연결함");
+    }
+}
